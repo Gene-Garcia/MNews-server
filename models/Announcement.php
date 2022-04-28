e59b80810ccccd7fe4f66232a0eedc53b4bca7b4
gn-grc <genejogarcia.gg@gmail.com>
2022-04-28 23:45:42 +0800

    added announcement crud logic

--- models/Announcement.php
+++ models/Announcement.php
@@ -1,16 +1,23 @@
 <?php 
 
+require "../database/DatabaseContext.php";
+
 class Announcement {
     private $id;
     private $subject;
     private $uploadDate;
     private $content;
 
+    // database
+    private $ctx;
+
     function __construct($idx, $subj, $upDate, $cnt){
         $this->id = $idx;
         $this->subject = $subj;
         $this->uploadDate = $upDate;
         $this->content = $cnt;
+
+        $this->ctx = new DatabaseContext();
     }
 
     // Getters and setters
@@ -30,22 +37,56 @@ class Announcement {
     // Database functions
     function uploadPost() {
         // check if all are populated
+        array_push($this->ctx->data()["Announcements"], $this);
     }
 
     public static function getAnnouncements(){
         // declare seperate db context
+        $dbCtx = new DatabaseContext();
+        return $dbCtx->data()["Announcements"];
     }
 
-    function getPost($idx){
-        
+    public static function getPost($idx){
+        $dbCtx = new DatabaseContext();
+
+        foreach($dbCtx->data()["Announcements"] as $value){
+            if ($value->getId() == $idx){
+                return $value;
+            }
+        }
+
+        return "Post not found";
     }
 
     function updatePost(){
         // check if all are populated
+        // pass reference
+        $data = &$this->ctx->data()["Announcements"];
+
+        $len = count($data);
+        for ($i = 0; $i < $len; $i++) {
+            if ($data[$i] == $this->id) {
+                $data[$i] = $this;
+                return "Post updated successfully";
+            }
+        }
+
+        return "Post not found and was not updated";
     }
 
     function deletePost(){
+        // pass reference
+        $data = &$this->ctx->data()["Announcements"];
 
+        $len = count($data);
+        for ($i = 0; $i < $len; $i++) {
+            if ($data[$i] == $this->id) {
+                unset($data[$i]);
+                return "Post found and deleted successfully";
+            }
+        }
+
+        return "Post not found and was not deleted";
     }
 
 }
