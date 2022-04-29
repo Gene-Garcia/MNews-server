3eb58ff02447a900a5e90d8e0794704abc0f586a
gn-grc <genejogarcia.gg@gmail.com>
2022-04-29 11:03:58 +0800

    added seperate announcement context for crud operations

---
+++ database/entity/AnnouncementContext.php
@@ -0,0 +1,62 @@
+<?php 
+
+/*
+ * A database context for announcement and its CRUD operations
+ * 
+ */
+class AnnouncementContext {
+
+    private $key = "Announcements";
+
+    private $dbCtx;
+    private $announcements;
+
+    function __construct($db, $data){
+        $this->dbCtx = $db;
+        // pass by reference
+        $this->$announcements = &$dt[$key];
+    }
+
+    function createAnnouncement($model){
+        array_push($this->announcements, $model);
+    }
+
+    function getAnnouncements(){
+        return $this->announcements;
+    }
+
+    function getAnnouncement($idx){
+        foreach($this->announcements as $announcement){
+            if ($announcement->getId() == $idx){
+                return $announcement;
+            }
+        }
+
+        return "Announcement post not found";
+    }
+
+    function updateAnnouncement($model){
+        for($i=0; $i<count($this->announcements); $i++){
+            if ($this->announcements[$i]->getId() == $model->getId()){
+                $this->announcements[$i] = $model; 
+                return "Announcement updated successfully";
+            }
+        }
+
+        return "Announcement not found and was not updated";
+    }
+
+    function deleteAnnouncement($idx){
+        for($i=0; $i<count($this->announcements); $i++){
+            if ($this->announcements[$i]->getId() == $idx){
+                unset($this->announcements[$i]);
+                return "Announcement deleted";
+            }
+        }
+
+        return "Announcement not found and not deleted";
+    }
+
+}
+
+?>
