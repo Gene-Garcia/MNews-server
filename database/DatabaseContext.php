5a987682a606354d692f3d52a215a5da8ad2fbee
gn-grc <genejogarcia.gg@gmail.com>
2022-05-01 15:12:05 +0800

    added file handling functionalities

--- database/DatabaseContext.php
+++ database/DatabaseContext.php
@@ -17,6 +17,51 @@ class DatabaseContext {
         return $this->announcementCtx;
     }
 
+
+    // text file handling
+    public $file;
+
+    // file status
+    private $status = array(
+        "READ" => "r",
+        // Open a file for read/write. The existing data in file is preserved. File pointer starts at the end of the file. Creates a new file if the file doesn't exist
+        "ADD" => "a+",
+        "UPDATE" => "w+"
+        );
+
+    function validateFile(){
+        $isExisting = file_exists($_SERVER["DOCUMENT_ROOT"] . "/MNews/MNews-server" . "/database/announcement_repo.txt");
+        
+        if (!$isExisting){
+            // create file and add base data
+            $temp = fopen(
+                $_SERVER["DOCUMENT_ROOT"] . "/MNews/MNews-server" . "/database/announcement_repo.txt", 
+                "w"
+            );
+
+            fwrite(
+                $temp, 
+                "hfgd!4|Subject 4|Upload Date|Content 4"
+            );
+
+            fclose($temp);
+        }
+    }
+
+    function openFile($stat){
+        $this->validateFile();
+
+        $this->file = fopen(
+            $_SERVER["DOCUMENT_ROOT"] . "/MNews/MNews-server" . "/database/announcement_repo.txt", 
+            $this->status[$stat]
+        );
+    }
+
+    function closeFile(){
+        fclose($this->file);
+    }
+
 }
+
 
 ?>
